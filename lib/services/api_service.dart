b121c3eaa5cc52386719a53f335fc175be95e695
Likhilvamsi <likhilvamsi@ameyait.com>
2025-11-13 17:12:49 +0530

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://20.9.130.19:8000';
 
   static Future<Map<String, dynamic>?> loginUser(
       String email, String password, String role) async {
